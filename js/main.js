8c6675f30a3732c4432d837e595a212532e3ded4
R2D2 <ernesg93@gmail.com>
2025-02-11 02:02:06 -0500

    editando

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("hola")
+// Función para sumar dos números
+function sumarNumeros(a, b) {
+    return a + b;  // Hay un error: la variable 'B' debería ser 'b'
+}
+
+function multiplicarNumeros(a, b) {
+    return a * b; 
+}
+
+// Llamada a la función con dos números
+let resultado = sumarNumeros(3, 5);
+let multiplicar = multiplicarNumeros(3,5)
+
+console.log("El resultado de la suma es: " + resultado);
+console.log(multiplicar);
